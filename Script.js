1ee2e53b76aeb854b37d818fa208332c2bbad007
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-09-26 11:46:34 +0600

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-var person = {
-       firstName:"Rimol",
-       lastName:"Chakma",
-       id:19702056,
-   fullName:function(){
-   return this.firstName + "" + this.lastName ;
-   };
-console.log(person.fullName());
