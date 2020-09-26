fa6eca76cf5d07fd77e0ea40e49ccfbf73cd902c
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-09-26 11:40:21 +0600

    Create Script.js

---
+++ this.keyword/Script.js
@@ -0,0 +1,8 @@
+var person = {
+       firstName:"Rimol",
+       lastName:"Chakma",
+       id:19702056,
+   fullName:function(){
+   return this.firstName + "" + this.lastName ;
+   }
+console.log(person.fullName());
