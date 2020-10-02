ad3fda62b12dd5301cff08acff20a730272b0827
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-10-02 10:38:26 +0600

    Create Script.js

---
+++ Accessors/Getter/Script.js
@@ -0,0 +1,12 @@
+var person = { 
+          firstName: "John",
+          lastName: " Doe ",
+          language: "English",
+          get lang(){
+            return this.language;
+            }
+           };
+ console.log(person.lang);
+      
+
+        
