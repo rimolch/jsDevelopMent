d03acccd282bbefb8638c4bbc1f2f9fbd05a5829
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-10-02 15:16:51 +0600

    Create Script.js

---
+++ JavaScript object constructor/Method 2/Script.js
@@ -0,0 +1,11 @@
+function person2(first,last,age,eye){
+this.firstName = first;
+this.lastName = last;
+this.age = age;
+this.eyeColor = eye;
+this.name = function(){
+return this.firstName + " " + this.lastName ;
+};
+}
+var myFather = new person2("Kamini","Ronjon",55,"blue");
+console.log(" My father name is " + myFather.name());
