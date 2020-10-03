c6a3928047e1fb25639a34b8a623cbfe6ee44996
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-10-03 15:08:26 +0600

    Create Script.js

---
+++ Object.defineProperty/Script.js
@@ -0,0 +1,10 @@
+var person = { 
+        firstName : " John ",
+        lastName : " Doe ",
+        language : " English "
+        };
+Object.defineProperty(person,"language",{ value : "Cause";}
+)
+console.log(person.language);
+ 
+       
