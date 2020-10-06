c9dc9066b7e936fc28cd2238fdba7b7e7b75619b
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-10-06 07:47:08 +0600

    Update and rename JavaScript Function/Script.js to JavaScript Function/Multiple/Script.js

---
+++ JavaScript Function/Multiple/Script.js
@@ -0,0 +1,18 @@
+// method 1
+function makeMultiply(a,b){
+  return a * b ;
+  }
+var show = makeMultiply(5,6);
+console.log(show);// returns 30
+
+
+// method 2
+var x = function(a,b){
+  return a * b ;
+  }
+console.log(x(6,7));//returns 42
+
+
+// method 3
+var myFunction = new Function("v","t","return s = v * t ");
+console.log(myFunction(300,8));// returns 2400
